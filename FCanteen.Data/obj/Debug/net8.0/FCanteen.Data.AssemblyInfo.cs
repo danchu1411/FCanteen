30512871fdfeab5472c020978364c8dc0647e962
danchu1411 <dangctde180178@fpt.edu.vn>
2026-09-23 20:15:30 +0700

    chore: clean duplicate projects and build artifacts

--- FCanteen.Data/obj/Debug/net8.0/FCanteen.Data.AssemblyInfo.cs
+++ FCanteen.Data/obj/Debug/net8.0/FCanteen.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCanteen.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e443274ad3a65a9f0e16c79d8f403c9189f968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8faeb2c58a60bd8ca0de815b89ee84c197308b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCanteen.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCanteen.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/FCanteen.Data/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FCanteen.Data;
-
-public class Class1
-{
-
-}
