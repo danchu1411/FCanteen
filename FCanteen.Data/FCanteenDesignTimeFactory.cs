beead4d85ff6991d3cab2bd18d8736850d29bf79
danchu1411 <dangctde180178@fpt.edu.vn>
2026-09-23 20:23:23 +0700

    Add appsettings.json and EF Core design-time factory
    Added appsettings.json with FCanteen DB connection string. Introduced FCanteenDesignTimeFactory for EF Core tooling, reading config from appsettings.json. Updated AssemblyInformationalVersionAttribute and AssemblyInfoInputs.cache to reflect new version and metadata changes.

---
+++ FCanteen.Data/FCanteenDesignTimeFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FCanteen.Data;
+
+public class FCanteenDesignTimeFactory
+    : IDesignTimeDbContextFactory<FCanteenContext>
+{
+    public FCanteenContext CreateDbContext(string[] args)
+    {
+        var basePath = Directory.GetCurrentDirectory();
+
+        if (!File.Exists(
+                Path.Combine(basePath, "appsettings.json")))
+        {
+            basePath =
+                Path.Combine(basePath, "FCanteen.Data");
+        }
+
+        var configuration =
+            new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile(
+                    "appsettings.json",
+                    optional: false)
+                .Build();
+
+        var connectionString =
+            configuration.GetConnectionString("FCanteen")
+            ?? throw new InvalidOperationException(
+                "Connection string 'FCanteen' not found.");
+
+        var optionsBuilder =
+            new DbContextOptionsBuilder<FCanteenContext>();
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new FCanteenContext(
+            optionsBuilder.Options);
+    }
+}
--- FCanteen.Data/obj/Debug/net8.0/FCanteen.Data.AssemblyInfo.cs
+++ FCanteen.Data/obj/Debug/net8.0/FCanteen.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCanteen.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30512871fdfeab5472c020978364c8dc0647e962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4d8b03860616c1858352d0304f1ef1c6a00bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCanteen.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCanteen.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
